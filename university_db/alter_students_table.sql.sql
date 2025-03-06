4b66fcac06246960e1f96f504792069642407828
MildredArquiza <arquizamildred_bsit@plmun.edu.ph>
2025-03-06 13:30:38 +0800

    Submitting ADVDBMS Week 02 - Session 01 - Exercise 02

--- university_db/alter_students_table.sql.sql
+++ university_db/alter_students_table.sql.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 01, 2025 at 10:07 AM
+-- Generation Time: Mar 06, 2025 at 06:24 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
